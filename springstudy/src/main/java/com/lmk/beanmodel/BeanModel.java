e6f4f407609ca2c3025ac08045a80de8ed3869e2
limingkun <dangshazi@163.com>
2017-06-07 23:13:56 +0800

    add spring appcontext demo`

---
+++ springstudy/src/main/java/com/lmk/beanmodel/BeanModel.java
@@ -0,0 +1,7 @@
+package com.lmk.beanmodel;
+
+/**
+ * Created by lmk on 2017/6/7.
+ */
+public class BeanModel {
+}
---
+++ springstudy/src/main/java/com/lmk/springconfigread/XmlConfigRead.java
@@ -0,0 +1,7 @@
+package com.lmk.springconfigread;
+
+/**
+ * Created by lmk on 2017/6/7.
+ */
+public class XmlConfigRead {
+}
